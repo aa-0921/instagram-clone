33712271854d2624e03efefa0f94fc6699b4100b
akuto aoyama <aaaa.akuto@gmail.com>
2020-03-19 16:42:40 +0900

    Fix puma.rb

--- config/puma.rb
+++ config/puma.rb
@@ -19,7 +19,7 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 # Specifies the `pidfile` that Puma will use.
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
-Specifies the number of `workers` to boot in single mode.
+# Specifies the number of `workers` to boot in single mode
 # Workers are forked web server processes. If using threads and workers together
 # the concurrency of the application would be max `threads` * `workers`.
 # Workers do not work on JRuby or Windows (both of which do not support
@@ -37,7 +37,7 @@ Specifies the number of `workers` to boot in single mode.
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
 
-workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+# workers Integer(ENV['WEB_CONCURRENCY'] || 2)
 threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
 threads threads_count, threads_count
 
