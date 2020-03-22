c76e197b8c7458695d99bab573f1a320bfc2ffd2
akuto aoyama <aaaa.akuto@gmail.com>
2020-03-22 19:29:37 +0900

    Add delete-method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
 
   def index
     @users = User.paginate(page: params[:page])
@@ -37,6 +38,12 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url # ユーザー一覧に戻る。
+  end
+
   private
 
   def user_params
@@ -45,7 +52,7 @@ class UsersController < ApplicationController
 
   # ログインしていないユーザーだと、警告が表示され、ログイン画面に飛ばされる。
   def logged_in_user
-    unless logged_in?
+    unless logged_in? # !否定演算子でcurrent_userが空でないならtrue
       store_location # 元々表示しようとしていたページをセッションに記憶
       flash[:danger] = "Please log in"
       redirect_to login_url
@@ -56,5 +63,9 @@ class UsersController < ApplicationController
   def correct_user
     @user = User.find(params[:id])
     redirect_to(root_url) unless current_user?(@user)
+  end
+
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
   end
 end
