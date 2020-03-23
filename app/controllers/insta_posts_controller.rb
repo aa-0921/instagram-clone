6dbc9ae0b31818c1e40e33cc6acb3c0ea66871d3
akuto aoyama <aaaa.akuto@gmail.com>
2020-03-23 12:40:03 +0900

    Add destroy and berore_action method

--- app/controllers/insta_posts_controller.rb
+++ app/controllers/insta_posts_controller.rb
@@ -1,5 +1,6 @@
 class InstaPostsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
+  before_action :correct_user, only: :destroy
 
   def create
     @insta_post = current_user.insta_posts.build(insta_post_params)
@@ -13,11 +14,19 @@ class InstaPostsController < ApplicationController
   end
 
   def destroy
+    @insta_post.destroy
+    flash[:success] = "Post deleted"
+    redirect_to request.referrer || root_url
   end
 
   private
 
   def insta_post_params
     params.require(:insta_post).permit(:content)
+  end
+
+  def correct_user
+    @insta_post = current_user.insta_posts.find_by(id: params[:id])
+    redirect_to root_url if @insta_post.nil?
   end
 end
