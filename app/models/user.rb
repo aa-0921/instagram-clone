6809a7ad88d2fc5749923d89d93ecaf4dcd7ac5c
akuto aoyama <aaaa.akuto@gmail.com>
2020-03-24 11:15:11 +0900

    Add stats-feed

--- app/models/user.rb
+++ app/models/user.rb
@@ -49,7 +49,8 @@ class User < ApplicationRecord
   end
 
   def feed
-    InstaPost.where("user_id = ?", id)
+    following_ids = "SELECT followed_id FROM relationships WHERE follower_id = :user_id"
+    InstaPost.where("user_id IN (#{following_ids}) OR user_id = :user_id", user_id: id)
   end
 
   # 他のユーザーをフォローの括りに入れる。
