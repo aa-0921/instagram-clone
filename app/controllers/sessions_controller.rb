dcf6ba687758512869b4d945f4fb1aece972ed53
akuto aoyama <aaaa.akuto@gmail.com>
2020-03-28 00:07:26 +0900

    Delete 20200326085552_add_columns_to_users.rb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,7 +12,7 @@ class SessionsController < ApplicationController
 
     user = User.find_by(email: params[:session][:email].downcase)
 
-    # binding.pry
+    binding.pry
 
     if user && user.authenticate(params[:session][:password])
       log_in user
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_26_085552) do
+ActiveRecord::Schema.define(version: 2020_03_26_084954) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -52,13 +52,12 @@ ActiveRecord::Schema.define(version: 2020_03_26_085552) do
     t.string "password_digest"
     t.string "remember_digest"
     t.boolean "admin", default: false
+    t.string "uid"
+    t.string "provider"
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "provider"
-    t.string "uid"
-    t.string "image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
