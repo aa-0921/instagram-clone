de76d8c991cf4f15c9368e214c663e3ce0e88942
akuto aoyama <aaaa.akuto@gmail.com>
2020-03-27 15:31:47 +0900

    Add sessionhelper-method

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ Bundler.require(*Rails.groups)
 module InstagramClone
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
+    
     config.load_defaults 6.0
 
     config.generators do |g|
